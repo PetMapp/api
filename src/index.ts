e489c19b43d75318484b27aca65b46d8dc253cd1
Eduardo Oliveira <48637125+eduardo14rj@users.noreply.github.com>
2024-09-13 22:00:47 -0300

    montagem do firebaseService

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import AuthenticationController from './controllers/AuthController';
 
 const app = express();
 const port = 3000;
 
 //JSON
 app.use(express.json());
+
+app.use("/auth", AuthenticationController);
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando 🐶`);
---
+++ src/models/user.ts
@@ -0,0 +1,5 @@
+import fbEntity from "./fbEntity";
+
+export default interface user extends fbEntity {
+    nome: string
+}
