52da467b24187a352dad84d94835523afc2e0450
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-07-24 14:54:11 -0300

    🚮remove: post
    Redundância entre a entidade post e pet.

--- src/models/entities/post.ts
+++
@@ -1,9 +0,0 @@
-import fbEntity from "./fbEntity"
-
-export default interface post extends fbEntity {
-    userId: string
-    titulo: string
-    descricao: string
-    coleira: boolean
-    status?: string
-}
